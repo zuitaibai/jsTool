807e520aa67ad87fe0240101899b130bd8a78a82
zuitaibai <gonghan007@163.com>
2021-08-13 07:05:27 +0800

    sth. about tree

--- tree.js
+++ tree.js
@@ -4,14 +4,14 @@
  * @Autor: zjf
  * @Date: 2021-01-11 06:21:49
  * @LastEditors: zjf
- * @LastEditTime: 2021-01-11 12:02:52
+ * @LastEditTime: 2021-08-08 11:00:07
  */
 
 class Treer{
-    constructor(data, idField = 'id', parentIdField = 'parentId', childrenFild = 'children', ifFlat=undefined){
-        this._idfld = idField;
-        this._pIdfld = parentIdField;
-        this._childfld = childrenFild;
+    constructor(data = [], {id = 'id', pId = 'parentId', cId = 'children', ifFlat} = {}){
+        this._idField = id;
+        this._pIdField = pId;
+        this._childFild = cId;
         let isFlat = typeof ifFlat === 'undefined' ? (data.some(item => parentIdField in item) && data.every(item => !(childrenFild in item))) : ifFlat;
         this.data = isFlat ? this.toTree(data) : data;
     }
@@ -29,7 +29,7 @@ class Treer{
         let node, list = [...this.data];
         while (node = list.shift()) {
             func(node);
-            node[this._childfld] && list.push(...node[this._childfld]);
+            node[this._childFild] && list.push(...node[this._childFild]);
         }
     }
 
@@ -37,7 +37,7 @@ class Treer{
     readByDeepAsc(func) {
         this.data.forEach(data => {
             func(data);
-            data[this._childfld] && this.readByDeepAsc(data[this._childfld], func);
+            data[this._childFild] && this.readByDeepAsc(data[this._childFild], func);
         })
     }
     ////遍历: 深度优先-先序 (循环实现)
@@ -45,14 +45,14 @@ class Treer{
         let node, list = [...this.data]
         while (node = list.shift()) {
             func(node)
-            node[this._childfld] && list.unshift(...node[this._childfld])
+            node[this._childFild] && list.unshift(...node[this._childFild])
         }
     }
 
     //遍历: 深度优先-后序 (递归实现)
     readByDeepDesc(func) {
         this.data.forEach(data => {
-            data[this._childfld] && this.readByDeepDesc(data[this._childfld], func);
+            data[this._childFild] && this.readByDeepDesc(data[this._childFild], func);
             func(data);
         })
     }
@@ -61,26 +61,26 @@ class Treer{
         let node, list = [...this.data],
             i = 0
         while (node = list[i]) {
-            let childCount = node[this._childfld] ? node[this._childfld].length : 0
-            if (!childCount || node[this._childfld][childCount - 1] === list[i - 1]) {
+            let childCount = node[this._childFild] ? node[this._childFild].length : 0
+            if (!childCount || node[this._childFild][childCount - 1] === list[i - 1]) {
                 func(node)
                 i++
             } else {
-                list.splice(i, 0, ...node[this._childfld])
+                list.splice(i, 0, ...node[this._childFild])
             }
         }
     }
 
     //列表转树
     toTree(list) {
-        let info = list.reduce((map, node) => (map[node[this._idfld]] = node, map), {})
+        let info = list.reduce((map, node) => (map[node[this._idField]] = node, map), {})
         return list.filter(node => {
-            let f = info[node[this._pIdfld]];
+            let f = info[node[this._pIdField]];
             if(f){
-                f[this._childfld] = f[this._childfld] || [];
-                f[this._childfld].push(node);
+                f[this._childFild] = f[this._childFild] || [];
+                f[this._childFild].push(node);
             }
-            return !node[this._pIdfld]
+            return !node[this._pIdField]
         })
     }
 
@@ -89,7 +89,7 @@ class Treer{
         this.data.forEach(node => {
             result.push(node)
             node._level = _level + 1
-            node[this._childfld] && this.toList(node[this._childfld], result, _level + 1)
+            node[this._childFild] && this.toList(node[this._childFild], result, _level + 1)
         })
         return result
     }
@@ -97,10 +97,10 @@ class Treer{
     toList2() {
         let result = this.data.map(node => (node._level = 1, node))
         for (let i = 0; i < result.length; i++) {
-            if (!result[i][this._childfld]) continue
-            let list = result[i][this._childfld].map(node => (node._level = result[i]._level + 1, node))
+            if (!result[i][this._childFild]) continue
+            let list = result[i][this._childFild].map(node => (node._level = result[i]._level + 1, node))
             result.splice(i + 1, 0, ...list)
-            delete result[i][this._childfld]
+            delete result[i][this._childFild]
         }
         return result
     }
@@ -109,8 +109,8 @@ class Treer{
     findNode(func) {
         for (const data of this.data) {
             if (func(data)) return data
-            if (data[this._childfld]) {
-                const res = this.findNode(data[this._childfld], func)
+            if (data[this._childFild]) {
+                const res = this.findNode(data[this._childFild], func)
                 if (res) return res
             }
         }
@@ -120,10 +120,10 @@ class Treer{
     //查找节点路径
     findPath(func, path = []) {
         for (const data of this.data) {
-            path.push(data[this._idfld])
+            path.push(data[this._idField])
             if (func(data)) return path
-            if (data[this._childfld]) {
-                const findChildren = this.findPath(data[this._childfld], func, path)
+            if (data[this._childFild]) {
+                const findChildren = this.findPath(data[this._childFild], func, path)
                 if (findChildren.length) return findChildren
             }
             path.pop()
@@ -132,6 +132,11 @@ class Treer{
     }
 
     //2叉树
+
+
+    static go(...args){
+        return new Treer(...args);
+    }
 
 }
 
